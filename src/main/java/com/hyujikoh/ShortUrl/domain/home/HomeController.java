c28d7e112eba7fb7150f36a4663d61470f46b590
hyunjikoh <dvum0045@youhost.co.kr>
2024-07-11 17:53:18 +0900

    디렉토리 변경 및 tiny 로그 추가

--- src/main/java/com/hyujikoh/ShortUrl/domain/home/HomeController.java
+++ src/main/java/com/hyujikoh/ShortUrl/domain/home/HomeController.java
@@ -2,16 +2,17 @@ package com.hyujikoh.ShortUrl.domain.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.tinylog.Logger;
 
 /**
- * Author : hyujikoh
- * CreatedAt : 2024-04-16
+ * Author : hyujikoh.
+ * CreatedAt : 2024-04-16.
  * Desc :
  */
 @Controller
 public class HomeController {
     @GetMapping("")
-    public String homeMethod(){
+    public String homeMethod() {
         return "index.html";
     }
 }
